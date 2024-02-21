76718741d40842a12e68272f186d9d3ee7eb23db
Song Veasna <156062281+veasnasong@users.noreply.github.com>
2024-02-21 09:04:48 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args)
+    {
+                Sale sale1 = new Sale(CustomerType.PREMIUM, 100.0, 200.0);
+                Sale sale2 = new Sale(CustomerType.SILVER, 150.0, 250.0);
+
+                System.out.println("Total Bill for Sale 1: $" + sale1.getTotalBill());
+                System.out.println("Total Bill for Sale 2: $" + sale2.getTotalBill());
+            }
+
+
 }
